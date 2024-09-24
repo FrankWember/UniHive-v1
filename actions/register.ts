fd3f26015fc93143e55a2c9709a088f711fa46bf
FrankWember <wemberfrankd@gmail.com>
2024-09-24 17:20:06 +0100

    BACKEND: added auth server actions (Incomplete)

---
+++ actions/register.ts
@@ -0,0 +1,42 @@
+"use server";
+
+import * as z from "zod";
+import bcrypt from "bcryptjs";
+
+import { SignUpSchema } from "@/constants/zod";
+import { prisma } from "@/prisma/connection";
+import { getUserByEmail } from "@/utils/user";
+import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
+
+export const register = async (values: z.infer<typeof SignUpSchema>) => {
+  const validatedFields = SignUpSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return { error: "Invalid fields!" };
+  }
+
+  const { email, name, student_id, phone,  password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await prisma.user.create({
+    data: {
+      name: name,
+      email: email,
+      phone: phone,
+      matricule: student_id,
+      password: hashedPassword,
+    },
+  });
+
+  const verificationToken = await generateVerificationToken(email);
+  await sendVerificationEmail(verificationToken.email, verificationToken.token);
+
+  return { sucess: "Confirmation email sent!" };
+};
--- constants/zod.ts
+++ constants/zod.ts
@@ -2,15 +2,20 @@ import * as z from "zod"
 
 export const IDSignInSchema = z.object({
   student_id: z.string({required_error: "Please enter your student ID"})
-    .min(8, "You names should be at least 8 characters long")
-    .max(10, "Your student ID cannot exceed 10 characters long")
+    .min(8, "Minimum 8 characters required!")
+    .max(10, "Maximum 10 characters required!")
     .startsWith("800", "Invalid student ID"),
-  password: z.string({required_error: "You must use a password"}).min(8),
+  password: z.string({required_error: "You must use a password"})
+    .min(8, "Minimum 8 characters required!"),
+  code: z.optional(z.string()),
 })
 
 export const EmailSignInSchema = z.object({
-  email: z.string({required_error: "You must have an email"}).email(),
-  password: z.string({required_error: "You must use a password"}).min(8),
+  email: z.string({required_error: "You must have an email"})
+    .email("Invalid email address"),
+  password: z.string({required_error: "You must use a password"})
+    .min(8, "Minimum 8 characters required!"),
+  code: z.optional(z.string()),
 })
 
 export const SignUpSchema = z.object({
@@ -20,13 +25,24 @@ export const SignUpSchema = z.object({
     .min(8, "Please enter a valid phone number")
     .max(15, "Please enter a valid phone number"),
   student_id: z.string({required_error: "Please enter your student ID"})
-    .min(8, "You names should be at least 8 characters long")
-    .max(10, "Your student ID cannot exceed 10 characters long")
+    .min(8, "Minimum 8 characters required!")
+    .max(10, "Maximum 10 characters required!")
     .startsWith("800", "Invalid student ID"),
   email: z.string({required_error: "You must have an email"})
     .email("Invald email address"),
   password: z.string({required_error: "Please Enter your password"})
-    .min(8, "Your password must be at least 8 characters long"),
+    .min(8, "Minimum 8 characters required!"),
   confirm_password: z.string({required_error: "Please Enter your password"})
-    .min(8, "Your password must be at least 8 characters long")
+    .min(8, "Minimum 8 characters required!")
 })
+
+export const ResetSchema = z.object({
+  email: z.string().email({
+    message: "Invalid email address",
+  }),
+});
+
+export const NewPasswordSchema = z.object({
+  password: z.string({required_error: "Please Enter your password"})
+    .min(8, "Minimum 8 characters required!"),
+});
