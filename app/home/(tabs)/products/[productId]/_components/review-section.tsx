ada11359ee4e528783ad273cfa41bd9e17b250fb
FrankWember <wemberfrankd@gmail.com>
2025-01-01 01:05:40 +0100

    fix product review section err with rating count

--- app/home/(tabs)/products/[productId]/_components/review-section.tsx
+++ app/home/(tabs)/products/[productId]/_components/review-section.tsx
@@ -142,9 +142,9 @@ export function ReviewsSection({
           {[5, 4, 3, 2, 1].map((rating) => (
             <div key={rating} className="flex items-center space-x-2">
               <span className="flex text-sm w-14 text-muted-foreground">{rating} stars</span>
-              <Progress value={((ratingCounts[rating] || 0) / reviews.length * 100) || 0} className="h-2 w-full" />
+              <Progress value={ratingCounts && ratingCounts[rating] ? (ratingCounts[rating] / reviews.length * 100) || 0 : 0} className="h-2 w-full" />
               <span className="w-12 text-sm text-muted-foreground text-right">
-                {(((ratingCounts[rating] || 0) / reviews.length * 100).toFixed(0)) || 0}%
+                {ratingCounts && ratingCounts[rating] ? (((ratingCounts[rating] || 0) / reviews.length * 100).toFixed(0)) || 0 : 0}%
               </span>
             </div>
           ))}
