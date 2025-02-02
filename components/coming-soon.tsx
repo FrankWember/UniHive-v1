690bbbe7e19462e38b4b6ef092b49bda9dbb7c07
FrankWember <wemberfrankd@gmail.com>
2025-02-03 00:03:18 +0100

    updated coming soon

--- components/coming-soon.tsx
+++ components/coming-soon.tsx
@@ -1,9 +1,13 @@
+"use client"
+
 import React from 'react'
 import { Card, CardTitle, CardDescription, CardFooter, CardHeader } from './ui/card'
 import Link from 'next/link'
 import { Button } from './ui/button'
+import { useRouter } from 'next/navigation'
 
 export const ComingSoon = () => {
+    const router = useRouter()
   return (
     <div className="w-full h-full flex items-center justify-center">
         <Card className='p-3'>
@@ -11,10 +15,13 @@ export const ComingSoon = () => {
                 <CardTitle>Coming Soon</CardTitle>
                 <CardDescription>This feature is currently under development.</CardDescription>
             </CardHeader>
-            <CardFooter>
+            <CardFooter className='flex gap-3 justify-end'>
+                <Button variant='outline' onClick={() => router.back()}>
+                    Go Back
+                </Button>
                 <Link href="/home">
                     <Button>
-                        Go Back Home 
+                        Home Page 
                     </Button>
                 </Link>
             </CardFooter>
