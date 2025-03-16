3fa773211e1897dcf853a671aedd3c2ab4a8bf04
Frank Wember <127029813+FrankWember@users.noreply.github.com>
2025-03-16 00:45:49 -0500

    Update page.tsx

--- app/home/(tabs)/services/page.tsx
+++ app/home/(tabs)/services/page.tsx
@@ -41,13 +41,13 @@ const ServicesPage = async ({ searchParams }: { searchParams: { [key: string]: s
 
       <div className='p-2 max-w-screen'>
         <ScrollArea className="w-full whitespace-nowrap mt-20 px-4 md:px-4 bg-muted/5 shadow-sm scroll-smooth bg-background/100">
-          <div className='flex items-center w-max gap-4 md:gap-12 p-2 h-14'>
+          <div className='flex items-center w-max gap-8 md:gap-12 p-2 h-14'>
             {categories.map((category) => (
               <Link key={category.name} href={`/home/services?category=${category.name.toLocaleLowerCase()}`}>
                 <div className='group flex flex-col items-center justify-center gap-2'>
                   <Image src={category.link} alt={category.name} width={16} height={16} className='object-cover'/>
                   <h2 className='text-xs md:text-sm font-semibold text-center text-muted-foreground'>{category.name}</h2>
-                  <div className='w-full h-1 rounded-lg bg-background group-hover:bg-foreground'></div>
+                  <div className='w-full h-0.5 rounded-lg bg-background group-hover:bg-gray-800'></div>
                 </div>
               </Link>
             ))}
