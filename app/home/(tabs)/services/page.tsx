030058249e55325788da07ca34774b80f7074524
Frank Wember <127029813+FrankWember@users.noreply.github.com>
2025-03-16 00:21:24 -0500

    Update page.tsx

--- app/home/(tabs)/services/page.tsx
+++ app/home/(tabs)/services/page.tsx
@@ -40,8 +40,8 @@ const ServicesPage = async ({ searchParams }: { searchParams: { [key: string]: s
       <ServicesHeader />
 
       <div className='p-2 max-w-screen'>
-        <ScrollArea className="w-full whitespace-nowrap mt-18 px-4 md:px-4 bg-muted/5 shadow-sm scroll-smooth">
-          <div className='flex items-center w-max gap-4 md:gap-12 p-2 h-6'>
+        <ScrollArea className="w-full whitespace-nowrap mt-24 px-4 md:px-4 bg-muted/5 shadow-sm scroll-smooth">
+          <div className='flex items-center w-max gap-4 md:gap-12 p-2 h-12'>
             {categories.map((category) => (
               <Link key={category.name} href={`/home/services?category=${category.name.toLocaleLowerCase()}`}>
                 <div className='group flex flex-col items-center justify-center gap-2'>
