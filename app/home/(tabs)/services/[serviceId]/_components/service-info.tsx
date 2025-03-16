e2d1b0896e7d9cb4a67c95fa5879a167b8a56387
Frank Wember <127029813+FrankWember@users.noreply.github.com>
2025-03-16 02:42:31 -0500

    Update service-info.tsx

--- app/home/(tabs)/services/[serviceId]/_components/service-info.tsx
+++ app/home/(tabs)/services/[serviceId]/_components/service-info.tsx
@@ -179,9 +179,9 @@ export const ServiceInfo = ({ service, averageRating, reviews }: ServiceInfoProp
 
     return (
         <div className='flex flex-col gap-4 py-4 w-full px-4 mx-auto'>
-            <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-1">
                 <div className="flex justify-between">
-                    <p className="text-xl md:text-2xl font-semibold truncate">{service.name}</p>
+                    <p className="text-xl md:text-2xl font-semibold truncate truncate max-w-[30ch]">{service.name}</p>
                     <Button variant="outline" size="icon" onClick={handleLike} disabled={isLiking}>
                         {isLiking ? <Spinner /> : <Heart className={`h-4 w-4 ${isLiked ? 'fill-red-500 text-red-500' : ''}`} />}
                     </Button>
@@ -189,15 +189,15 @@ export const ServiceInfo = ({ service, averageRating, reviews }: ServiceInfoProp
                 <div className="flex justify-between">
                     <span className="flex items-end mt-2">
                         <span className='text-l mr-2'>Starts at</span>
-                        <span className='text-green-500 font-semibold text-2xl'>$</span>
-                        <span className="font-semibold text-2xl mr-3">
+                        <span className='text-green-600 font-semibold text-xl'>$</span>
+                        <span className="font-semibold text-xl mr-3">
                             {service.price.toFixed(2)}
                         </span>
                     </span>
                     
                 </div>
                 <div className="flex justify-between gap-3">
-                    <span className="flex items-center text-sm md:text-base truncate max-w-[40ch] ">
+                    <span className="flex items-center text-sm md:text-base truncate max-w-[25ch] ">
                         <MapPin className="mr-1 h-4 w-4" />
                         {service.defaultLocation}
                     </span>
