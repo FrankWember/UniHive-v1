0623871bd18fd43977aae2775d17fc88d4582e75
Frank Wember <127029813+FrankWember@users.noreply.github.com>
2025-03-26 09:28:08 -0500

    Update booking-form.tsx
    Time zone adjustment

--- app/home/(tabs)/services/[serviceId]/book/[offerId]/_components/booking-form.tsx
+++ app/home/(tabs)/services/[serviceId]/book/[offerId]/_components/booking-form.tsx
@@ -7,6 +7,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import * as z from 'zod'
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
 import { Button } from '@/components/ui/button'
+import { parseISO, format } from 'date-fns';
 import {
   Select,
   SelectContent,
@@ -175,7 +176,7 @@ export function BookingForm({ offerId, service, offer }: BookingFormProps) {
                     <SelectItem 
                       key={index} 
                       value={`${slot[0]} - ${slot[1]}`}>
-                      {`${(new Date("1970-01-01T" + slot[0].replace(/-/g, ":"))).toLocaleTimeString([], { hour: 'numeric', minute: '2-digit', hour12: true })} to ${(new Date("1970-01-01T" + slot[1].replace(/-/g, ":"))).toLocaleTimeString([], { hour: 'numeric', minute: '2-digit', hour12: true })}`}
+                      {`${parseISO(`1970-01-01T${slot[0].replace(/-/g, ':')}`).toLocaleTimeString([], { hour: 'numeric', minute: '2-digit', hour12: true })} to ${parseISO(`1970-01-01T${slot[1].replace(/-/g, ':')}`).toLocaleTimeString([], { hour: 'numeric', minute: '2-digit', hour12: true })}`}
                     </SelectItem>
                   )) : (
                     <div>
