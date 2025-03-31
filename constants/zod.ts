0a4d1396c80755640cad8aee83bc1816a7c6f469
Frank Wember <127029813+FrankWember@users.noreply.github.com>
2025-03-31 18:43:13 -0500

    Update zod.ts

--- constants/zod.ts
+++ constants/zod.ts
@@ -76,8 +76,8 @@ export const ServiceSchema = z.object({
     message: "Please select at least one category.",
   }),
   images: z.array(z.string())
-  .min(1, {
-    message: "Please upload at least one image.",
+  .min(5, {
+    message: "Please upload at least 5 images.",
   })
   .max(5, {
     message: "You can upload a maximum of 5 images.",
