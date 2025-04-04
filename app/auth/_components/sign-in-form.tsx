2ce2b550b3e23b8e4f77bba792f219c368fe4d0b
Frank Wember <127029813+FrankWember@users.noreply.github.com>
2025-04-03 21:24:53 -0500

    Update sign-in-form.tsx

--- app/auth/_components/sign-in-form.tsx
+++ app/auth/_components/sign-in-form.tsx
@@ -82,9 +82,8 @@ const EmailForm = () => {
             form.reset();
             setSuccess(data.success);
 
-            //router.push(callbackUrl || "/home/services");
-           router.push("/home/services");
-           window.location.reload();
+           router.push(callbackUrl);
+           router.refresh();
           }
 
           if (data?.twoFactor) {
