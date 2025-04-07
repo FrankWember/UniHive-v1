9dbcf1cafaf953b47787de351ec0f370f9699f94
Frank Wember <127029813+FrankWember@users.noreply.github.com>
2025-04-06 19:38:05 -0500

    memo fixed

--- app/home/(tabs)/inbox/_components/chat-interface.tsx
+++ app/home/(tabs)/inbox/_components/chat-interface.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef, useMemo} from 'react'
 import { motion } from 'framer-motion'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
